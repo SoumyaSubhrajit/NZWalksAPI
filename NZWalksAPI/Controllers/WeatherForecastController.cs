a9edd59845d088c36c3af7494d07f64175b23ed4
Soumya Subhrajit Bag <soumyasubhrajit@gmail.com>
2024-09-03 10:31:51 +0530

    Update WeatherForecastController.cs

--- NZWalksAPI/Controllers/WeatherForecastController.cs
+++ NZWalksAPI/Controllers/WeatherForecastController.cs
@@ -18,7 +18,7 @@ namespace NZWalksAPI.Controllers
         {
             _logger = logger;
         }
-        // Adding something in the 
+        // Adding something
 
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
