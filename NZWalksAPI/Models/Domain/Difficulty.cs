0f063e95ecd943217c4d15ff4a0bcdc5ff243030
Soumya Subhrajit Bag <soumyasubhrajit@gmail.com>
2024-09-04 14:36:40 +0530

    Update Difficulty.cs

--- NZWalksAPI/Models/Domain/Difficulty.cs
+++ NZWalksAPI/Models/Domain/Difficulty.cs
@@ -5,6 +5,6 @@
         public Guid Id { get; set; }
 
         public string Name { get; set; }
-        // This is the thing and there is no way we h
+        // This is the thing and there is no way
     }
 }
